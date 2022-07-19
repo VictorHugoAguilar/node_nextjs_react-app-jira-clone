b9816119791d891535c2211aa7f4dfbe1e3f10bf
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-19 22:58:21 +0200

    add themes in main class

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
 import type { AppProps } from 'next/app'
+import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import '../styles/globals.css'
 
+const basicTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={basicTheme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
 
 export default MyApp
