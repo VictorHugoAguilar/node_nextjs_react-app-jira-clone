c0214fde4d0bc726256d11b13f1234b8d9969b59
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-20 01:21:27 +0200

    create uireducer
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- context/ui/uiReducer.ts
+++ context/ui/uiReducer.ts
@@ -1,4 +1,21 @@
+import { UIState } from "./UIProvider";
 
-export const uiReducer = () => {
-    
-}
+type UIActionType =
+    | { type: 'UI - Open Sidebar' }
+    | { type: 'UI - Close Sidebar' }
+
+export const uiReducer = (state: UIState, action: UIActionType): UIState => {
+    switch (action.type) {
+        case 'UI - Open Sidebar':
+            return {
+                ...state,
+                sideMenuOpen: true,
+            }
+        case 'UI - Close Sidebar':
+            return {
+                ...state,
+                sideMenuOpen: false,
+            }
+        default:
+            return state;
+    }
