bb6d9cd0bb875cc83ecb0b73afbf721dc42cb231
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-20 13:46:46 +0200

    Update NewEntry.tsx
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- components/ui/NewEntry.tsx
+++ components/ui/NewEntry.tsx
@@ -6,60 +6,99 @@ import SaveOutlinedIcon from '@mui/icons-material/SaveOutlined';
 import AddIcon from '@mui/icons-material/AddCircleOutline';
 
 export const NewEntry = () => {
+
+    const [isAdding, setIsAdding] = React.useState(false);
+
+    const [inputTitle, setInputTitle] = React.useState('');
+    const [inputDescription, setInputDescription] = React.useState('');
+    const [touched, setTouched] = React.useState(false);
+
+    const onTextFieldTitleChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputTitle(event.target.value);
+    }
+
+    const onTextFieldDescriptionChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputDescription(event.target.value);
+    }
+
+    const onSaved = () => {
+        if (inputTitle.length === 0 || inputDescription.length === 0) return;
+
+        console.log(`Saving new entry: ${inputTitle}`);
+        console.log(`Saving new entry: ${inputDescription}`);
+    }
+
     return (
         <Box sx={{
             marginBottom: 2,
             paddingX: 1
         }}>
-            <Button
-                startIcon={<AddIcon />}
-                fullWidth
-                variant='outlined'
-            >
-                Agregar nueva tarea
-            </Button>
+            {isAdding ? (
+                <>
+                    <TextField
+                        fullWidth
+                        sx={{
+                            marginTop: 2,
+                            marginBottom: 1
+                        }}
+                        placeholder="Title"
+                        autoFocus
+                        multiline
+                        label='Título Nueva entrada'
+                        variant="outlined"
+                        helperText={inputTitle.length <= 0 && touched && "Escribe el título de la entrada"}
+                        error={inputTitle.length <= 0 && touched}
+                        value={inputTitle}
+                        onChange={onTextFieldTitleChanged}
+                        onBlur={() => { setTouched(true) }}
+                    />
+                    <TextField
+                        fullWidth
+                        sx={{
+                            marginTop: 2,
+                            marginBottom: 1
+                        }}
+                        placeholder="Descripción"
+                        multiline
+                        label='Descripció Nueva entrada'
+                        variant="outlined"
+                        helperText={inputDescription.length <= 0 && touched && "Escribe la descripción de la entrada"}
+                        value={inputDescription}
+                        onChange={onTextFieldDescriptionChanged}
+                        onBlur={() => { setTouched(true) }}
+                        error={inputDescription.length == 0 && touched}
+                    />
 
-            <TextField
-                fullWidth
-                sx={{
-                    marginTop: 2,
-                    marginBottom: 1
-                }}
-                placeholder="Title"
-                autoFocus
-                multiline
-                label='Título Nueva entrada'
-                variant="outlined"
-                helperText="Escribe el título de la entrada"
-            />
-            <TextField
-                fullWidth
-                sx={{
-                    marginTop: 2,
-                    marginBottom: 1
-                }}
-                placeholder="Descripción"
-                autoFocus
-                multiline
-                label='Descripció Nueva entrada'
-                variant="outlined"
-                helperText="Escribe la descripción de la entrada"
-            />
+                    <Box display="flex" justifyContent="space-between" >
 
-            <Box display="flex" justifyContent="space-between" >
+                        <Button
+                            variant='outlined'
+                            color='secondary'
+                            endIcon={<CloseIcon />}
+                            onClick={() => { setIsAdding(false) }}
+                        > Cancelar</Button>
 
-                <Button
-                    variant='outlined'
-                    color='secondary'
-                    endIcon={<CloseIcon />}
-                > Cancelar</Button>
-
-                <Button
-                    variant='outlined'
-                    color='secondary'
-                    endIcon={<SaveOutlinedIcon />}
-                > Añadir</Button>
-            </Box>
-        </Box>
+                        <Button
+                            variant='outlined'
+                            color='secondary'
+                            endIcon={<SaveOutlinedIcon />}
+                            onClick={onSaved}
+                        > Añadir</Button>
+                    </Box>
+                </>
+            ) : (
+                <>
+                    < Button
+                        startIcon={<AddIcon />}
+                        fullWidth
+                        variant='outlined'
+                        onClick={() => { setIsAdding(true) }}
+                    >
+                        Agregar nueva tarea
+                    </Button>
+                </>
+            )
+            }
+        </Box >
     )
 }
