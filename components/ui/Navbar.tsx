717045d4ed72dbbc2cb213a6f0dff53a571dc1ef
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-20 09:37:01 +0200

    implements context for open and close menu sidebar
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,14 +1,22 @@
+import { useContext } from "react";
+
 import { AppBar, IconButton, Toolbar, Typography } from "@mui/material"
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
 
+import { UIContext } from "../../context/ui";
+
 
 export const Navbar = () => {
+
+    const { openSideMenu } = useContext(UIContext);
+
     return (
         <AppBar position="sticky">
             <Toolbar>
                 <IconButton
                     size="large"
                     edge='start'
+                    onClick={() => { openSideMenu() }}
                 >
                     <MenuOutlinedIcon />
                 </IconButton>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import { CssBaseline, ThemeProvider } from '@mui/material';
 import '../styles/globals.css'
 
 import { lightTheme, darkTheme } from '../theme';
+import { UIProvider } from '../context/ui';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
   )
 }
 
