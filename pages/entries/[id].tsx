0d269c3a558c0b094e3d8bd946f0062a0ee975b1
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-21 22:34:18 +0200

    add style page entry new task
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- pages/entries/[id].tsx
+++ pages/entries/[id].tsx
@@ -1,3 +1,4 @@
+import { useMemo, useState } from 'react';
 import { NextPage } from 'next';
 
 import { Button, capitalize, Card, CardActions, CardContent, CardHeader, FormControl, FormControlLabel, FormLabel, Grid, IconButton, Radio, RadioGroup, TextField } from '@mui/material';
@@ -10,12 +11,51 @@ import { EntryStatus } from '../../interfaces';
 const validStatus: EntryStatus[] = ['pending', 'in-progress', 'finished'];
 
 const EntryPage: NextPage = () => {
+
+    const [inputTitle, setInputTitle] = useState('');
+    const [inputDescription, setInputDescription] = useState('');
+    const [inputStatus, setInputStatus] = useState<EntryStatus>('pending');
+    const [touched, setTouched] = useState(false);
+
+    const onTextFieldTitleChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputTitle(event.target.value);
+    }
+
+    const onTextFieldDescriptionChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setInputDescription(event.target.value);
+    }
+
+    const onStatusChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(`Status changed to: ${event.target.value}`);
+        setInputStatus(event.target.value as EntryStatus);
+    }
+
+    const isNotValidTitle = useMemo(() => inputTitle.length <= 0 && touched, [inputTitle, touched]);
+    const isNotValidDescription = useMemo(() => inputDescription.length <= 0 && touched, [inputDescription, touched]);
+
+    const onSaved = () => {
+        // if (inputTitle.length === 0 || inputDescription.length === 0) return;
+
+        // console.log(`Saving new entry: ${inputTitle}`);
+        // console.log(`Saving new entry: ${inputDescription}`);
+
+        // // Saved in context
+        // addNewEntry(inputTitle, inputDescription);
+
+        // // Reset form
+        // setInputTitle('');
+        // setInputDescription('');
+        // setTouched(false);
+        // setIsAddingEntry(false);
+    }
+
+
     return (
         <Layout title="Entry Page">
             <Grid container justifyContent={'center'} sx={{ marginTop: '1rem' }}     >
                 <Grid item xs={12} sm={8} md={6} >
                     <Card>
-                        <CardHeader title='Entrada: ' subheader={'creada hace ...'} />
+                        <CardHeader title={`Entrada: ${inputTitle}`} subheader={'creada hace ...'} />
 
                         <CardContent>
                             <TextField
@@ -24,6 +64,11 @@ const EntryPage: NextPage = () => {
                                 placeholder='Nueva entrada'
                                 autoFocus
                                 label='Titulo Nueva entrada'
+                                value={inputTitle}
+                                onChange={onTextFieldTitleChanged}
+                                onBlur={() => { setTouched(true) }}
+                                helperText={isNotValidTitle && "Escribe el titulo de la entrada"}
+                                error={isNotValidTitle}
                             />
 
                             <TextField
@@ -33,12 +78,19 @@ const EntryPage: NextPage = () => {
                                 autoFocus
                                 multiline
                                 label='Descripcion Nueva entrada'
+                                value={inputDescription}
+                                onChange={onTextFieldDescriptionChanged}
+                                helperText={isNotValidDescription && "Escribe la descripcion de la entrada"}
+                                onBlur={() => { setTouched(true) }}
+                                error={isNotValidDescription}
                             />
 
                             <FormControl>
                                 <FormLabel>Estado:</FormLabel>
                                 <RadioGroup
                                     row
+                                    value={inputStatus}
+                                    onChange={onStatusChanged}
                                 >
                                     {
                                         validStatus.map(status => (
@@ -57,6 +109,8 @@ const EntryPage: NextPage = () => {
                                     startIcon={<SaveOutlinedIcon />}
                                     variant="contained"
                                     fullWidth
+                                    onClick={onSaved}
+                                    disabled={inputTitle.length <= 0 || inputDescription.length <= 0}
                                 >Guardar</Button>
                             </CardActions>
 
