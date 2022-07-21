cf0ec6fb36f084b7b404a56ef5bc7c59be691107
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-22 01:10:35 +0200

    Update EntryList.tsx
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- components/ui/EntryList.tsx
+++ components/ui/EntryList.tsx
@@ -7,6 +7,8 @@ import { EntryStatus } from '../../interfaces'
 import { EntryListItem } from './EntryListItem'
 import { UIContext } from '../../context/ui'
 
+import { showLogs } from '../../utils'
+
 import styles from './EntryList.module.css'
 
 interface Props {
@@ -22,11 +24,12 @@ export const EntryList: FC<Props> = ({ status }) => {
 
     const allowDrop = (e: DragEvent<HTMLDivElement>) => {
         e.preventDefault()
-        console.log(e.dataTransfer.getData('text'))
+        // showLogs('info', 'in allowDrop')
     }
 
     const onDropEntry = (e: DragEvent<HTMLDivElement>) => {
         const id = e.dataTransfer.getData('text')
+        showLogs('info', 'in onDropEntry with id:', id)
 
         const entry = entries.find(entry => entry._id === id)!;
         entry.status = status;
@@ -36,12 +39,31 @@ export const EntryList: FC<Props> = ({ status }) => {
     }
 
     return (
-        <div onDrop={onDropEntry} onDragOver={allowDrop} className={isDragging ? styles.dragging : ''}  >
-            <Paper sx={{ height: 'calc(100vh - 180px)', overflow: 'scroll', backgroundColor: 'transparent', padding: '3px 5px' }}>
-                <List sx={{ opacity: isDragging ? 0.2 : 1, transition: 'all .3s' }}>
+        <div
+            onDrop={onDropEntry}
+            onDragOver={allowDrop}
+            className={isDragging ? styles.dragging : ''}
+        >
+            <Paper
+                sx={{
+                    height: 'calc(100vh - 180px)',
+                    overflow: 'scroll',
+                    backgroundColor: 'transparent',
+                    padding: '3px 5px'
+                }}
+            >
+                <List
+                    sx={{
+                        opacity: isDragging ? 0.2 : 1,
+                        transition: 'all .3s'
+                    }}
+                >
                     {
                         entiesByStatus.map(entry => (
-                            <EntryListItem key={entry._id} entry={entry} />
+                            <EntryListItem
+                                key={entry._id}
+                                entry={entry}
+                            />
                         ))
                     }
                 </List>
