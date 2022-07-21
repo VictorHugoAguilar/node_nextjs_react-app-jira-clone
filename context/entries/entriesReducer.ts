408bc2b8d5948d2bfe4753992754939bbf77b6dc
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-21 17:40:49 +0200

    create refresh data
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- context/entries/entriesReducer.ts
+++ context/entries/entriesReducer.ts
@@ -4,6 +4,7 @@ import { EntriesState } from './';
 type entriesActionType =
     | { type: '[Entry] - Add-Entry', payload: Entry }
     | { type: '[Entry] - Update-Entry', payload: Entry }
+    | { type: '[Entry] - Refresh-Entry', payload: Entry[] }
 
 
 export const entriesReducer = (state: EntriesState, action: entriesActionType): EntriesState => {
@@ -26,6 +27,12 @@ export const entriesReducer = (state: EntriesState, action: entriesActionType): 
                     }
                     return entry;
                 })
+            }
+
+        case '[Entry] - Refresh-Entry':
+            return {
+                ...state,
+                entries: [...action.payload],
             }
         default:
             return state;
