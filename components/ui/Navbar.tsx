0eedd0316529513eddf539ad54ff1d0fed94cd45
Victor Hugo Aguilar <37599330+VictorHugoAguilar@users.noreply.github.com>
2022-07-22 10:10:25 +0200

    Update Navbar.tsx
    Co-Authored-By: VictorHugoAguilarAguilar <37845751+VHAA1718@users.noreply.github.com>

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,8 +1,10 @@
 import { useContext } from "react";
 import NextLink from "next/link";
 
-import { AppBar, IconButton, Link, Toolbar, Typography } from "@mui/material"
+import { AppBar, Grid, IconButton, Link, Toolbar, Typography } from "@mui/material"
 import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import NightlightIcon from '@mui/icons-material/Nightlight';
+import LightModeIcon from '@mui/icons-material/LightMode';
 
 import { UIContext } from "../../context/ui";
 
@@ -11,33 +13,54 @@ export const Navbar = () => {
     const { openSideMenu } = useContext(UIContext);
 
     return (
-        <AppBar position="sticky">
-            <Toolbar>
-                <IconButton
-                    size="large"
-                    edge='start'
-                    onClick={() => { openSideMenu() }}
-                >
-                    <MenuOutlinedIcon />
-                </IconButton>
-
-                <NextLink
-                    href="/"
-                    passHref
-                >
-                    <Link
-                        underline="none"
-                        color='white'
-                    >
-                        <Typography
-                            variant="h6"
+        <AppBar position="sticky" >
+            <Grid container display={'flex'} justifyContent={'space-between'}>
+                <Grid item>
+                    <Toolbar >
+                        <IconButton
+                            size="large"
+                            edge='start'
+                            onClick={() => { openSideMenu() }}
                         >
-                            OpenJira
-                        </Typography>
-                    </Link>
-                </NextLink>
+                            <MenuOutlinedIcon />
+                        </IconButton>
 
-            </Toolbar>
+                        <NextLink
+                            href="/"
+                            passHref
+                        >
+                            <Link
+                                underline="none"
+                                color='white'
+                            >
+                                <Typography
+                                    variant="h6"
+                                >
+                                    OpenJira
+                                </Typography>
+                            </Link>
+                        </NextLink>
+                    </Toolbar>
+                </Grid>
+                <Grid item
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignContent={'center'}
+                    alignItems={'center'}
+                    sx={{
+                        marginRight: '1rem'
+                    }}
+                >
+                    <IconButton
+                        size="medium"
+                        edge='start'
+                        onClick={() => { openSideMenu() }}
+                    >
+                        <LightModeIcon />
+
+                    </IconButton>
+                </Grid>
+            </Grid>
         </AppBar>
     )
 }
